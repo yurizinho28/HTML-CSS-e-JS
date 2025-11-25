b220ef191f3d5001db1d0022c60c906f117ff7bd
Yurix <yurisantosbrito2010@gmail.com>
2025-11-25 20:18:02 -0300

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,7 @@
+let userName = document.querySelector('.userName');
+let senha = document.querySelector('.senha');
+let login = document.querySelector('.login');
+
+login.addEventListener('submit', function(enviar) {
+    enviar.preventDefault();
+})
